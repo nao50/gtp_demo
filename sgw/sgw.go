ea42f968bec1096860119a385de0261fb5a818d9
naoyamaguchi <naoyamaguchi0128@gmail.com>
2018-05-05 06:01:25 +0900

    edit

---
+++ sgw/sgw.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"syscall"
+)
+
+func main() {
+	///////////////////////////////////////////////////////////////////////////////////////
+	// common
+	const proto = (syscall.ETH_P_IP<<8)&0xff00 | syscall.ETH_P_IP>>8
+	buffer := make([]byte, 1550)
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// S1:DOWNLINK:Send:RawSocket
+	///////////////////////////////////////////////////////////////////////////////////////
+	// S5:DOWNLINK:Recv:GTPv1Decap
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// S1:UPLINK:Recv:RawSocket
+	recvSockFd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_DGRAM, proto)
+	if err != nil {
+		log.Fatal("recvSockFd: ", err)
+	}
+	defer syscall.Close(recvSockFd)
+
+	recvIf, err := net.InterfaceByName("eth0")
+	if err != nil {
+		log.Fatal("interfacebyname: ", err)
+	}
+
+	recvSll := syscall.SockaddrLinklayer{
+		Protocol: proto,
+		Ifindex:  recvIf.Index,
+	}
+
+	if err := syscall.Bind(recvSockFd, &recvSll); err != nil {
+		log.Fatal("bind: ", err)
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// S5:UPLINK:Send:GTPv1Encap
+	conn, err := net.Dial("udp4", "10.0.11.20:2152")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// main loop
+	fmt.Println("Starting raw server...")
+	for {
+		n, addr, err := syscall.Recvfrom(recvSockFd, buffer, 0)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// FOR DEBUG
+		sa, _ := addr.(*syscall.SockaddrLinklayer)
+		fmt.Printf("Recv SockaddrLinklayer: %+v\n", sa)
+
+		go func() {
+			g := &GTPV1{
+				Version:                 1,
+				ProtocolType:            1,
+				Reserved:                0,
+				ExtensionHeaderFlag:     0,
+				SequenceNumberFlag:      1,
+				N_PDUNumberFlag:         0,
+				MessageType:             255,           // GPDU
+				MessageLength:           uint16(n + 4), // よくわからん
+				TEID:                    16879116,
+				SequenceNumber:          65530,
+				N_PDUNumber:             0,
+				NextExtensionFeaderType: 0,
+				Data: buffer[:n],
+			}
+			msg := g.Marshal(buffer[:n])
+
+			_, err = conn.Write(msg)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}()
+	}
+}
